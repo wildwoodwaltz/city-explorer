070b5b3d2bd70ad2b0f70d7192ae7dec3539142e
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-03-21 14:14:22 -0700

    added framework for city explorer application

--- src/Cityform.js
+++ src/Cityform.js
@@ -1,32 +1,60 @@
 import React from  'react';
-import Form, { FormGroup, FormLabel, FormText } from 'react-bootstrap'
+import Form, { FormGroup, FormLabel, FormText } from 'react-bootstrap';
+import axios from 'axios';
 
 class Cityform extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      city: ''
+      city: '',
+      cityData: '',
+      error: false,
+      errorMessage: ''
     };
-  }
+  };
   
 handleCityInput = (e) => {
- let citySubmit = e.target.value;
-  this.setState ({
-    city: citySubmit,
-  });
-};
+// get data from API
+
+// save data into state
+this.setState({
+  city: e.target.value
+});
+}
+handleSubmit = async (e) => {
+  e.preventDefault();
+try{
+  let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+this.setState({
+  cityData: cityData.data
+});
+} catch (error) {
+  console.log('error', error.response);
+  this.setState({
+    eroor: true,
+    errorMessage: `An error has been caught: ${error.response.status}`
+  })
+}
+}
 
   render(){
+    let cityDataValues = this.state.city.map((acc, key) => {
+      return <li key={key}>{acc}</li>
+    })
   return (
+    <>
     <FormGroup>
-      <FormLabel>Pick a City</FormLabel>
+      <FormLabel>Pick a City:</FormLabel>
       <FormText 
-      onChange={this.handleCityInput}>
-
-      </FormText>
+      onChange={this.handleCityInput}/>
+      <Form.Button onSubmit={this.handleSubmit}>Get City Data</Form.Button>
     </FormGroup>
-       
+       <ul>
+        <li>{cityDataValues}</li>
+       </ul>
+    </>
   );
 }
 }
+
 export default Cityform;
